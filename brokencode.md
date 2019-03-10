89f0b4f31aa046f48e3ae0d5d2310e16597fe4a0
mbmbamfan <davidanddavid@outlook.com>
2019-03-10 13:34:21 -0400

    Update

---
+++ brokencode.md
@@ -0,0 +1,48 @@
+#This is where code that does not work but can offer functionality goes
+
+**LEXER CODE**
+elif tok == "call:":
+        tokens.append("CALL")
+        tok = ""
+elif tok == "end":
+      tokens.append("END")
+      tok = ""
+  elif tok == "function":
+      #if var != "":
+      #    tokens.append("NAME:" + var)
+      #    var = ""
+      tokens.append("FUNCTION")
+      tok = ""
+  elif tok == "do":
+      tokens.append("DO")
+      tok = ""
+
+
+
+
+
+
+
+
+
+
+
+**PARSER CODE**
+if toks[i][0:4] + " " + toks[i+1][0:3] == "CALL VAR":
+        print(getVARIABLE(toks[i+1][4:]))
+        i+=1
+
+if toks[i] == "END":
+        #print("end")
+        i+=1
+    elif toks[i] + " " + toks[i+1][0:3] + " " + toks[i+2] == "FUNCTION VAR DO":
+        if toks[i+1][0:4] == "VAR:":
+            t = 0
+            while t < len(toks):
+                if(toks[t] == "DO"):
+                    #print(toks[i+1][4:])
+                    #print(toks[t+1:])
+                    doASSIGN(toks[i+1][4:], toks[t+1:])
+                t+=1
+        i+=3
+        #return
--- main/basic.py
+++ main/basic.py
@@ -12,8 +12,6 @@ def lex(filecontents):
     state = 0
     isexpr = 0
     varstarted = 0
-    func = ""
-    isfunc = 0
     var = ""
     string = ""
     expr = ""
@@ -52,6 +50,8 @@ def lex(filecontents):
             varstarted = 1
             var += tok
             tok = ""
+
+
         elif varstarted == 1:
             if tok == "<" or tok == ">":
                  if var != "":
@@ -65,22 +65,6 @@ def lex(filecontents):
             #print("print")
             tokens.append("PRINT")
             tok = ""
-        #Basis for functions
-        #unfinished (Currently not working)
-        elif tok == "{" and state == 0:
-            isfunc = 1
-            func += tok
-            tok = ""
-        elif isfunc == 1:
-            if tok == "<" or tok == ">":
-                if func != "":
-                    tokens.append("CODE:" + func)
-                    func = ""
-                    isfunc = 0
-            func += tok
-            tok = ""
-
-
         elif tok == "0" or tok == "1" or tok == "2" or tok == "3" or tok == "4" or tok == "5" or tok == "6" or tok == "7" or tok == "8" or tok == "9":
             expr += tok
             tok = ""
@@ -99,7 +83,7 @@ def lex(filecontents):
         elif state == 1:
             string += tok
             tok = ""
-    print(tokens)
+    #print(tokens)
     #print(symbols)
     #return ""
     return tokens
@@ -123,41 +107,6 @@ def getVARIABLE(varname):
         #return symbols[varname]
     else:
         return "TypeError: Undefined Variable"
-evaledarray = []
-def evalCODE(code):
-
-    code = code[6:]
-    txt = code.find("}")
-    eol = code.find(";")
-    line = code[:eol]
-    count = code.count(";")
-    line = code.split(";")
-    i = 0
-    while i <= len(line) - 1:
-        if line[i] != "}":
-            if line[i].count("\"") > 0:
-                end = line[i][7:].find("\"")
-                txt = line[i][7:]
-                evaledarray.append(txt[:end])
-            elif line[i].count("+") > 0 or line[i].count("-") > 0 or line[i].count("*") > 0 or line[i].count("/") > 0:
-                txt = eval(line[i][6:])
-                evaledarray.append(txt)
-            else:
-                evaledarray.append(line[i][6:])
-        i+=1
-        return ""
-    #if eol != 0:
-    #    line2 = code[eol:]
-    #    txt = line2.find("}")
-    #    line2 = line2[1:txt]
-    #    line2 = line2[7:]
-    #    line1 = code[:eol]
-    #    line1 = line1[7:]
-    #    block = line1 + "\n" + line2
-    #    print(block)
-    #else:
-    #    print(line1)
-    return evaledarray
 def parse(toks):
     i = 0
     while(i < len(toks)):
@@ -172,7 +121,7 @@ def parse(toks):
             elif toks[i+1][0:3] == "VAR":
                 print(getVARIABLE(toks[i+1][4:]))
             i+=2
-        elif toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:6] == "VAR EQUALS STRING" or toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:3] == "VAR EQUALS NUM" or toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:4] == "VAR EQUALS EXPR" or toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:4] == "VAR EQUALS CODE":
+        elif toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:6] == "VAR EQUALS STRING" or toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:3] == "VAR EQUALS NUM" or toks[i][0:3] + " " + toks[i+1] + " " + toks[i+2][0:4] == "VAR EQUALS EXPR":
             if toks[i+2][0:6] == "STRING":
                 doASSIGN(toks[i][4:],  toks[i+2][8:])
             if toks[i+2][0:3] == "NUM":
@@ -180,11 +129,10 @@ def parse(toks):
             if toks[i+2][0:4] == "EXPR":
                 #print(toks[i+2])
                 doASSIGN(toks[i][4:], evalEXPRESSION(toks[i+2]))
-            if toks[i+2][0:4] == "CODE":
-                doASSIGN(toks[i][4:], evalCODE(toks[i+2]))
             i+=3
+
             #print(symbols)
-        #i+=1
+
 def run():
     data = open_file(argv[1])
     tok = lex(data)
